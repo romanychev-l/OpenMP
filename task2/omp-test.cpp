9ccc16137d4f507082129d84252703aec44742f3
romanychev-l <leonid20.0831@gmail.com>
2021-12-09 12:51:02 +0300

    Add tasks 2 and 3

--- task2/omp-test.cpp
+++ task2/omp-test.cpp
@@ -2,60 +2,76 @@
 #include <stdio.h>
 #include <iostream>
 #include <bits/stdc++.h>
+#include <iomanip>
 
 using namespace std;
 
 typedef long long ll;
 typedef long double ld;
+typedef vector<vector<int> > vv;
+typedef vector<int> v;
 
+template <typename T>
+string to_string_with_precision(const T a_value, const ll n = 6)
+{
+    ostringstream out;
+    out.precision(n);
+    out << fixed << a_value;
+    return out.str();
+}
 
-void vprint(vector<ll>& a){
-	for(ll i = 0; i<a.size(); i++){
-		cout << a[i] << ' ';
+void vprint(ll* array, ll n){
+	for(ll i = 0; i<n; i++){
+		cout << array[i] << ' ';
 	}
 	cout << "\n";
 }
 
+ll scalar(ll* array1, ll* array2, ll n, ll num_threads=1){
+    ll result = 0;
+	omp_set_num_threads(num_threads);
+	#pragma omp parallel for reduction(+:result)
+	for (int i = 0; i < n; ++i)
+        	result += (array1[i] * array2[i]);
+
+    return result;
+}
+
+void gen_v(ll* array, ll n){
+	for(ll i = 0; i < n; i++){
+		array[i] = rand() % 1000 + 1;
+    }
+}
 
 int main () {
-    omp_set_num_threads(8);
+    for(ll j = 4; j<9; j++){
+        ll n = pow(10, j);
 
-	ll n, ans = 0, s = 0;
+        ll* array1 = new ll[n];
+        ll* array2 = new ll[n];
+        v results;
+        gen_v(array1, n);
+        gen_v(array2, n);
 
-	cin >> n;
+        cout << "Количество потоков: " << j << "\n";
+        for(ll i = 1; i <= 16; i *= 2){
+            ld start_time = omp_get_wtime();
+            ll result = scalar(array1, array2, n, i);
+            ld time = omp_get_wtime() - start_time;
 
-	vector<ll> a(n), b(n);
+            results.push_back(result);
+            string t = to_string_with_precision(time, 8);
+            t[1] = ',';
+            cout << t << '\n';
+            //cout << fixed << setprecision(8) << t << "\n";
+            //cout << time << '\n';
+        }
+        for(ll i = 0; i<results.size(); i++){
+            cout << results[i] << ' ';
+        }
+        cout << "\n";
+    }
 
-	for(ll i = 0; i<n; i++){
-		a[i] = rand() % 10000;
-		b[i] = rand() % 10000;
-	}
 
-	//vprint(a);
-	//vprint(b);
-
-	ll start_time = clock();
-
-	for(ll i = 0; i<n; i++){
-		ans += a[i] * b[i];
-	}
-
-	ll end_time = clock();
-
-	cout << "time: " << end_time - start_time << "\n";
-
-	start_time =  clock();
-
-#pragma omp parallel for reduction(+:s)
-	for(ll i = 0; i<n; i++){
-		s += a[i] * b[i];
-	}
-
-	end_time = clock();
-
-	cout << "time: " << end_time - start_time << "\n";
-	cout << "ans = " << ans << "\n";
-	cout << "s = " << s << "\n";
-
-	return 0;
+    return 0;
 }
